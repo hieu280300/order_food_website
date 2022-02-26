cc4c17e64be3c57bd4b502161db8fe7008435106
hieu2803 <hieutran2803@gmail.com>
2022-02-26 16:00:12 +0700

    create website

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \App\Models\User::factory(10)->create();
+        // \App\Models\Admin::factory(10)->create();
+        // $this->call(CategorySeeder::class);
+        // $this->call(ProductSeeder::class);
+        // $this->call(PriceSeeder::class);
+        
+        // $this->call(ColorSeeder::class);
+        // $this->call(SizeSeeder::class);
+        // $this->call(ImageSeeder::class);
+        // $this->call(PromotionSeeder::class);
+    }
+}
