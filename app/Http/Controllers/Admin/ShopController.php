cdcc6c10ed9a7ea8ef6951d5489ec1acadba6b6c
hieu2803 <hieutran2803@gmail.com>
2022-04-05 15:53:26 +0700

    update database

--- app/Http/Controllers/Admin/ShopController.php
+++ app/Http/Controllers/Admin/ShopController.php
@@ -13,7 +13,7 @@ class ShopController extends Controller
         $shops = DB::table('users')
         ->leftJoin('shops', 'users.id', '=', 'shops.user_id')
         ->where('users.role','=','1')
-        ->select('users.name as user_name','shops.name as shop_name','shops.image as shop_image','shops.id as shop_id','shops.address as shop_address','users.role as user_role','users.email as user_email')->get();
+        ->select('users.name as user_name','shops.name as shop_name','shops.image as shop_image','shops.id as shop_id','shops.address as shop_address','users.role as user_role','users.email as user_email')->paginate(5);
     
         $data['shops'] = $shops;
         return view('admin.auth.shops.index', $data);
